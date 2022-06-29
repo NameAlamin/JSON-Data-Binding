ec3cf2d5db7a019ba14d51d97301ebbea457dcab
Alamin <alamin8740@gmail.com>
2022-06-29 11:46:03 +0600

    add java POJO class ( Student -> set variable and generate getter setter)

---
+++ src/main/java/com/Student.java
@@ -0,0 +1,40 @@
+package com;
+
+public class Student {
+    int id;
+    String firstName;
+    String lastName;
+    boolean active;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
