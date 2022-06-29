9e37fd066a45a285e1bf2eec93b645232ba7372d
Alamin <alamin8740@gmail.com>
2022-06-29 12:55:33 +0600

    convert JSON to-> JAVA-POJO (nested-json-object and json-array)

--- src/main/java/com/ProjectRunnerClass.java
+++ src/main/java/com/ProjectRunnerClass.java
@@ -12,13 +12,27 @@ public class ProjectRunnerClass {
             ObjectMapper mapper = new ObjectMapper();
 
             // read json file and convert to java POJO
-            Student student1 = mapper.readValue(new File("data/sample-lite.json"),Student.class);
+            Student student1 = mapper.readValue(new File("data/sample-full.json"),Student.class);
 
             // now print individual item
+            System.out.println("=========== Print basic information ===========");
             System.out.println("Id = "+student1.getId());
             System.out.println("FirstName = "+student1.getFirstName());
             System.out.println("LastName = "+student1.getLastName());
-            System.out.println("Active = "+student1.active);
+            System.out.println("Active = "+student1.isActive());
+
+            // now print Address: street and city
+            System.out.println("=========== Print Address ===========");
+            Address printAddress = student1.getAddress();
+            System.out.println("Street = "+printAddress.getStreet());
+            System.out.println("City = "+printAddress.getCity());
+
+            //now print languages array
+            System.out.println("=========== Print Languages array ===========");
+            for (String printLanguage:student1.getLanguages()) {
+                System.out.println(printLanguage);
+            }
+
         }catch (Exception e)
         {
             e.printStackTrace();
--- src/main/java/com/Student.java
+++ src/main/java/com/Student.java
@@ -1,10 +1,22 @@
 package com;
 
 public class Student {
-    int id;
-    String firstName;
-    String lastName;
-    boolean active;
+    private int id;
+    private String firstName;
+    private String lastName;
+    private boolean active;
+    private Address address;
+    private String[] languages;
+
+
+    /*
+        *** now we understand if json file all variable/name not present inside java-pojo-class
+            it gives and Exception
+
+        *** and if java-pojo-class don't generate getter and setter again it gives an Exception Because
+        i) when we convert JSON to-> JAVA-POJO jackson call {setter method} by default
+        ii) when we convert JAVA-POJO to-> JSON jackson call {getter method} by default
+     */
 
     public int getId() {
         return id;
@@ -36,5 +48,21 @@ public class Student {
 
     public void setActive(boolean active) {
         this.active = active;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String[] getLanguages() {
+        return languages;
+    }
+
+    public void setLanguages(String[] languages) {
+        this.languages = languages;
     }
 }
